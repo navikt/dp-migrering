17bb13916fdc95e139823162274a951356717121
Lars Erik Gabestad <lars.erik.gabestad2@nav.no>
2026-09-15 07:51:04 +0200

    WIP: Endret til å ta med alle i spørring

--- arena-migrering/src/main/kotlin/no/nav/dagpenger/migrering/arena/migrering/ArenaRepository.kt
+++ arena-migrering/src/main/kotlin/no/nav/dagpenger/migrering/arena/migrering/ArenaRepository.kt
@@ -72,15 +72,49 @@ class ArenaRepository(
                        ON av.VEDTAK_ID = vv.VEDTAK_ID
                 WHERE  vv.VILKAARSTATUSKODE != 'V'
             )
-            SELECT VILKAARKODE,
-                   VILKAARSTATUSKODE,
-                   MOD_DATO,
+            SELECT vilkaarkode,
+                   vilkaarstatuskode,
+                   mod_dato,
                    nivaa
             FROM   alle_vilkaarvurderinger
             WHERE  rad_nummer = 1
             ORDER  BY VILKAARKODE
-                """.trimIndent(),
+            """.trimIndent(),
             mapOf("vedtakId" to vedtakId),
+        ) { row ->
+            ArenaSakMigrering.ArenaVilkaarMigrering(
+                kode = row.string("vilkaarstatuskode"),
+                verdi = row.string("vilkaarkode"),
+                gyldigFra = row.localDate("mod_dato"),
+            )
+        }
+        return select
+    }
+
+    fun hentSakVilkaarvurdering(
+        sakId: Int,
+    ): List<ArenaSakMigrering.ArenaVilkaarMigrering> {
+        val select = select(
+            // language=oracle
+            """
+            SELECT vilkaarkode
+                 , vilkaarstatuskode
+                 , mod_dato
+            FROM   vilkaarvurdering
+            WHERE  vilkaarvurdering_id IN (
+                     SELECT FIRST_VALUE( vilk.vilkaarvurdering_id ) OVER ( PARTITION BY vilk.vilkaarkode ORDER BY vedt.fra_dato DESC ) vilkaarvurdering_id
+                     FROM   vedtak vedt
+                     JOIN   vilkaarvurdering vilk ON vilk.vedtak_id = vedt.vedtak_id
+                     WHERE  vedt.sak_id = :sakId
+                     AND    vedt.rettighetkode IN ('DAGO','PERM','LONN','FISK')
+                     AND    vedt.utfallkode = 'JA'
+                     AND    vedt.fra_dato <= NVL(vedt.til_dato, vedt.fra_dato)
+                     AND    vedt.vedtaktypekode IN ('O','E','G','S')
+                     AND    vedt.vedtakstatuskode IN ('IVERK', 'AVSLU')
+                     AND    vilk.vilkaarstatuskode IN ('J', 'N')
+                   )
+            """.trimIndent(),
+            mapOf("sakId" to sakId),
         ) { row ->
             ArenaSakMigrering.ArenaVilkaarMigrering(
                 kode = row.string("vilkaarstatuskode"),
